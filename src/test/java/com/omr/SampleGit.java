701c9dc4d65b4d5460be8f946e57b7c166bdbb6e
akilangithub <akilanr14082000@gmail.com>
2026-02-17 21:22:45 +0530

    Initial commit

--- src/test/java/com/omr/SampleGit.java
+++ src/test/java/com/omr/SampleGit.java
@@ -1,31 +1,46 @@
 package com.omr;
 
 public class SampleGit {
-	
+
 	private void method1() {
-		
+
 		System.out.println("method1");
 
 	}
+
 	private void method2() {
-		
+
 		System.out.println("method2");
 
 	}
+
 	private void method3() {
-		
+
 		System.out.println("method3");
 
 	}
-	
+
+	private void method4() {
+
+		System.out.println("method4");
+
+	}
+
+	private void method5() {
+
+		System.out.println("method5");
+
+	}
+
 	public static void main(String[] args) {
-		
+
 		SampleGit git = new SampleGit();
 		git.method1();
 		git.method2();
 		git.method3();
-		
+		git.method4();
+		git.method5();
+
 	}
-	
 
 }
