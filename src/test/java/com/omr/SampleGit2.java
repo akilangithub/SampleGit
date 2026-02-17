923c008a9e64677300059a21a606fda3e0eb7779
akilangithub <akilanr14082000@gmail.com>
2026-02-17 21:34:38 +0530

    Initial commit

---
+++ src/test/java/com/omr/SampleGit2.java
@@ -0,0 +1,30 @@
+package com.omr;
+
+public class SampleGit2 {
+
+	private void method11() {
+
+		System.out.println("method1");
+
+	}
+
+	private void method21() {
+
+		System.out.println("method2");
+
+	}
+
+	private void method31() {
+
+		System.out.println("method3");
+
+	}
+
+	public static void main(String[] args) {
+
+		SampleGit2 git2 = new SampleGit2();
+		git2.method11();
+		git2.method21();
+		git2.method31();
+	}
+}
